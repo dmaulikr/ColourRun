a4d32a32ab0c66a84111755cb6d409fa88446596
Ian <git@ianplumb.org>
2014-04-26 22:20:03 +0100

    Added ColourGrid

---
+++ ColourRun/CRColourGrid.h
@@ -0,0 +1,13 @@
+//
+//  CRColourGrid.h
+//  ColourRun
+//
+//  Created by Ian on 26/04/2014.
+//  Copyright (c) 2014 Ian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRColourGrid : NSObject
+
+@end
---
+++ ColourRun/CRColourGrid.m
@@ -0,0 +1,13 @@
+//
+//  CRColourGrid.m
+//  ColourRun
+//
+//  Created by Ian on 26/04/2014.
+//  Copyright (c) 2014 Ian. All rights reserved.
+//
+
+#import "CRColourGrid.h"
+
+@implementation CRColourGrid
+
+@end
